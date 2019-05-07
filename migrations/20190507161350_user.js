767c84ffb61384e838bf26da745e2845dd62a55b
unknown <syedazhim10@gmail.com>
2019-05-07 16:24:08 +0800

    initial commit

---
+++ migrations/20190507161350_user.js
@@ -0,0 +1,16 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', (table)=>{
+        table.increments()
+        table.text('display_name').notNullable()
+        table.text('email').notNullable()
+        table.text('google_id').notNullable()
+        table.boolean('banned').notNullable().default(false)
+        
+    })
+
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('users')
+};
